00c929254792ff7dfcfa3a4f262194b3042c149d
Staliho <staliho@nwcs.com.hk>
2025-04-18 16:42:43 +0800

    feat: update profile card

--- app/src/main/java/com/example/comp4521_ustrade/app/components/ProfileCard.kt
+++ app/src/main/java/com/example/comp4521_ustrade/app/components/ProfileCard.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,6 +15,8 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
@@ -46,7 +49,22 @@ fun ProfileCard(modifier: Modifier = Modifier, ProfileCardData : ProfileCardData
                     .clip(androidx.compose.foundation.shape.CircleShape),
                 contentScale = androidx.compose.ui.layout.ContentScale.Crop
             )
-            Text(text = ProfileCardData.name, modifier = modifier.weight(3f), fontSize = 24.sp, fontWeight = FontWeight.Bold)
+
+            Column(modifier = modifier.weight(3f), verticalArrangement = Arrangement.SpaceAround) {
+                Text(text = ProfileCardData.name, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                Spacer(modifier.padding(2.dp))
+                Surface(
+                    color = Color(0xFF90EE90),
+                    shape = MaterialTheme.shapes.small
+                ) {
+                    Text(
+                        text = "Lv.1 Contributor",
+                        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                        color = Color.Black
+                    )
+                }
+            }
+
             IconButton(onClick = {}) {
                 Icon(
                     imageVector = Icons.Default.IosShare,
