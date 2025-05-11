a6862845bed5d8fe8e0270a76466aace7378c436
Staliho <staliho@nwcs.com.hk>
2025-05-11 14:58:34 +0800

    fix: edit profile sze

--- app/src/main/java/com/example/comp4521_ustrade/app/screens/settings/EditProfileScreen.kt
+++ app/src/main/java/com/example/comp4521_ustrade/app/screens/settings/EditProfileScreen.kt
@@ -3,6 +3,7 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.CameraAlt
@@ -38,6 +40,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -148,6 +151,7 @@ fun EditProfileScreen(
                 modifier = Modifier
                     .size(150.dp)
                     .align(Alignment.CenterHorizontally)
+                    .clip(CircleShape)
             ) {
                 if (isUploading) {
                     CircularProgressIndicator(
@@ -168,7 +172,7 @@ fun EditProfileScreen(
                                 contentDescription = stringResource(R.string.ProfilePicture),
                                 modifier = Modifier.fillMaxSize(),
                                 contentScale = ContentScale.Crop,
-                                error = painterResource(id = R.drawable.default_profile_pic)  // Fallback to default image
+                                error = painterResource(id = R.drawable.default_profile_pic)
                             )
                         } else {
                             Image(
@@ -185,7 +189,9 @@ fun EditProfileScreen(
                             modifier = Modifier
                                 .align(Alignment.BottomEnd)
                                 .padding(8.dp)
-                                .size(32.dp),
+                                .size(32.dp)
+                                .background(Color.Black.copy(alpha = 0.3f), CircleShape)
+                                .padding(4.dp),
                             tint = Color.White
                         )
                     }
